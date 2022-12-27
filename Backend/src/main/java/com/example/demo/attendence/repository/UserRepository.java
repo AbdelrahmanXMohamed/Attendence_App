708e86ca2cc9e5a41495015a8028703ce7d67edc
Mohamed Ali Nossir <71195812+Mohammed011233@users.noreply.github.com>
2022-12-27 09:39:54 +0200

    the crud services of users

--- Backend/src/main/java/com/example/demo/attendence/repository/UserRepository.java
+++ Backend/src/main/java/com/example/demo/attendence/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.attendence.repository;
 
 import com.example.demo.attendence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
