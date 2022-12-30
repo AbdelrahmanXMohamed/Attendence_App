4fc90fad83170588e9301f5ba3f4d3c95680e3e6
ayaashraf14 <aya.ashraf.selim123@gmail.com>
2022-12-30 10:04:47 +0200

    init

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
