9b3995b9afd36a342719015d353eabd4f22fde8f
Abdelrahman Mohamed <abdelrahman.mohamedx1999@gmail.com>
2023-01-01 17:04:05 +0200

    abdrahman ui 10000000000% done

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
